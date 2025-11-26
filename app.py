c1f60d8db414d19588aecaa95c293a9aebab92d0
VAISHNAVI7558 <payalrayphale@gmail.com>
2025-11-26 08:19:50 +0000

    commit

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 # Write your code here
+import streamlit as st
+import pandas as pd
+from utils import predict_species
+import joblib
+
+model = joblib.load('/workspaces/iris-Streamlit-Deployment-/notebook/iris_model.joblib')
+
+# Design page
+
+st.set_page_config(page_title = 'Iris end to end deploymebt')
+
+# Give title
+st.title("Iris Deployment for end user")
+
+# Give Name
+st.subheader('By Vaishnavi Rayphale')
--- utils.py
+++ utils.py
@@ -1,1 +1,22 @@
 # Write your code here
+from sklearn.pipeline import Pipeline
+import pandas as pd
+import streamlit
+
+## Create funtion to predict output of  species
+def predict_species(model : Pipeline, 
+                    sep_len : int|float ,
+                    sep_wid : int|float ,
+                    petal_len : int|float ,
+                    petal_wid : int|float ):
+    
+    x_new = pd.DataFrame([{'sepal_leaght' : sep_len ,
+                           "sepal_width" : sep_wid ,
+                           "petal_len" : petal_len ,
+                           "petal_width" : petal_wid}])
+    
+
+    pred = model.predict(x_new)
+    prob = model.predict_proba(x_new)
+    probs = pd.DataFrame(prob , columns=model.classes_)
+    return pred , probs
